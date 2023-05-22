31ce247a0804b90512f3a22870ff2e2966765a15
Filipa Merino <filipaam@outlook.com>
2023-05-22 13:19:21 +0100

    Change props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,9 @@ function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card />
+      <Card name="Filipa" img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" tel="+123 456 789" email="b@beyonce.com" />
+      <input id="fName" placeholder="Enter your first name"/>
+
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,18 +2,22 @@ import React from 'react'
 
 
 
-function Card(){
+function Card(props){
+
+  console.log(props)
+
+
   return(
     <div className='card'>
+
       <div className='top'>
-        <h2>Beyonce</h2>
-        <img
-          src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-          alt="avatar_img"/>
+        <h2>{props.name}</h2>
+        <img src={props.img}/>
       </div>
+
       <div className='bottom'>
-        <p>+123 456 789</p>
-        <p>b@beyonce.com</p>
+        <p>{props.tel}</p>
+        <p>{props.email}</p>
 
       </div>
     </div>
@@ -23,4 +27,3 @@ function Card(){
 
 
 export default Card;
-
