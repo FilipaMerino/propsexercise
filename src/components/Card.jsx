5693ed04bd208e9c7cee06903894c039ca324c4a
Filipa Merino <filipaam@outlook.com>
2023-05-22 16:08:59 +0100

    styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,13 +8,13 @@ function Card(props){
   return(
     <div className='card'>
       <div className='top'>
-        <h2>{props.name}</h2>
+        <h2 className='name'>{props.name}</h2>
         <img src={props.img}/>
       </div>
 
       <div className='bottom'>
-        <p>{props.tel}</p>
-        <p>{props.email}</p>
+        <p className='info'>{props.tel}</p>
+        <p className='info'>{props.email}</p>
 
       </div>
     </div>
