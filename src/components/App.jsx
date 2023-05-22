5f2f2b94bf8da56beff58c34dc7af22dcbd9caf0
Filipa Merino <filipaam@outlook.com>
2023-05-22 16:20:15 +0100

    contacts variable

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 import Card from "./Card";
+import contacts from "../contacts"
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card name="Tommy" img="https://images.pexels.com/photos/1687831/pexels-photo-1687831.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" tel="+123 456 789" email="b@beyonce.com" />
-      <Card name="Pipoca" img="https://images.pexels.com/photos/1851164/pexels-photo-1851164.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" tel="I have no phone, I'm a dog duuuh" email="dog@gmail.com" />
-      <Card name="Cusquinho" img="https://images.pexels.com/photos/2928158/pexels-photo-2928158.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" tel="I have no phone, I'm a cat duuuh" email="cat@gmail.com" />
+      <Card
+        name= {contacts[0].name}
+        img = {contacts[0].imgURL}
+        tel = {contacts[0].phone}
+        email = {contacts[0].email}
+
+      />
+
+
+    <Card
+      name= {contacts[1].name}
+      img = {contacts[1].imgURL}
+      tel = {contacts[1].phone}
+      email = {contacts[1].email}
+    />
+
+    <Card
+      name= {contacts[2].name}
+      img = {contacts[2].imgURL}
+      tel = {contacts[2].phone}
+      email = {contacts[2].email}
+    />
       <input id="fName" placeholder="Enter your first name"/>
 
 
---
+++ src/contacts.js
@@ -0,0 +1,29 @@
+const contacts = [
+
+{
+  name:"Tommy",
+  imgURL:"https://images.pexels.com/photos/1687831/pexels-photo-1687831.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  phone:"I'm a cat, I don't have a phone",
+  email:"CATMAIL@gmail.com",
+},
+
+{
+  name:"Marley",
+  imgURL:"https://images.pexels.com/photos/1851164/pexels-photo-1851164.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  phone:"I'm a dog, I don't have a phone",
+  email:"KittyCat@gmail.com",
+},
+
+
+{
+  name:"Cusquinho",
+  imgURL:"https://images.pexels.com/photos/2928158/pexels-photo-2928158.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  phone:"I'm a cat, I don't have a phone",
+  email:"EatSleepRepeat@gmail.com",
+},
+
+
+]
+
+
+export default contacts;
